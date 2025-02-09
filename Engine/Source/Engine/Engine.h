0439d48cc6a88b3ef12069824b78f34259f9078d
Olowerr <oliver.klingspor@hotmail.com>
2025-02-09 14:59:20 +0100

    added dependencies

--- Engine/Source/Engine/Engine.h
+++ Engine/Source/Engine/Engine.h
@@ -3,6 +3,12 @@
 #include <cstdio>
 #include <d3d12.h>
 
+#include "assimp/Importer.hpp"
+#include "entt/entt.hpp"
+#include "GLFW/glfw3.h"
+#include "glm/glm.hpp"
+#include "stb/stb_image.h"
+
 namespace Okay
 {
 	void HelloWorld();
